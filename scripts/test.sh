88a1684262c57710e807ea654f0bfadb7b61e253
bofm <bofm.gh@gmail.com>
2019-08-08 12:30:06 +0300

    remove unused code

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,12 +5,4 @@ set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 cd "$ROOT"
 
-consul_config_file=/etc/consul/consul.hcl
-mkdir -p /etc/consul
-
-cat > "$consul_config_file" <<"EEE"
-disable_anonymous_signature = true
-disable_update_check = true
-EEE
-
 tarantool "scripts/run_tests.lua" --pattern "^test_.*%.lua$" "${ROOT}/tests"
